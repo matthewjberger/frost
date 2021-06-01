6f41e7da0f3a8bd70e06c43bbffd9c602ec991f4
Matthew J. Berger <matthewberger@nevada.unr.edu>
2021-06-01 00:30:25 -0600

    Evaluate function objects

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,10 @@ mod lexer;
 mod parser;
 
 pub use self::{evaluator::*, lexer::*, parser::*};
+
+use std::fmt::Display;
+
+fn flatten(items: &[impl Display], separator: &str) -> String {
+    let strings = items.iter().map(|s| s.to_string()).collect::<Vec<_>>();
+    format!("{}", strings.join(separator))
+}
